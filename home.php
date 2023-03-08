f7bddbc481140159ccc0bfe817590b401acc878f
Dn1San <c0009613@hallam.shu.ac.uk>
2023-03-08 14:58:01 +0000

    Home

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home || Group 42</title>
+</head>
+<body>
+    <header> 
+        <nav>
+            <ul>
+                <li><a href="home.php">HOME</a></li>
+                <li><a href="create.php">Create Tutor/Student</a></li>
+                <li><a href="permissions.php">Permissions</a></li>
+                <li><a href="progress.php">Progress</a></li>
+            </ul>
+            <div id="sign-out">
+                <a href="login.php">Sign Out</a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
