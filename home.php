eb320456f7ead6acb10ad035da5f20b262e0bce5
Dn1San <c0009613@hallam.shu.ac.uk>
2023-03-08 15:06:50 +0000

    Header

--- home.php
+++ home.php
@@ -7,18 +7,8 @@
     <title>Home || Group 42</title>
 </head>
 <body>
-    <header> 
-        <nav>
-            <ul>
-                <li><a href="home.php">HOME</a></li>
-                <li><a href="create.php">Create Tutor/Student</a></li>
-                <li><a href="permissions.php">Permissions</a></li>
-                <li><a href="progress.php">Progress</a></li>
-            </ul>
-            <div id="sign-out">
-                <a href="login.php">Sign Out</a>
-            </div>
-        </nav>
-    </header>
+    <?php
+        include("includes/header.inc.php");
+    ?>
 </body>
 </html>
---
+++ includes/header.inc.php
@@ -0,0 +1,13 @@
+<header> 
+        <nav>
+            <ul>
+                <li><a href="home.php">HOME</a></li>
+                <li><a href="create.php">Create Tutor/Student</a></li>
+                <li><a href="permissions.php">Permissions</a></li>
+                <li><a href="progress.php">Progress</a></li>
+            </ul>
+            <div id="sign-out">
+                <a href="login.php">Sign Out</a>
+            </div>
+        </nav>
+</header>
